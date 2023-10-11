9173eaf98db5af9dd2ce44c0e33314a66096b266
JaseGrable <jase.grable@gmail.com>
2023-10-11 14:33:45 -0700

    Add Auth component and sign in page

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -5,23 +5,23 @@ import './navigation.styles.scss'
 
 const Navigation = () => {
     return (
-    <Fragment>
+      <Fragment>
         <div className='navigation'>
-            <Link className="logo-container" to='/'>
-            <CrwnLogo className="logo" />
+          <Link className='logo-container' to='/'>
+            <CrwnLogo className='logo' />
+          </Link>
+          <div className='nav-links-container'>
+            <Link className='nav-link' to='/shop'>
+              SHOP
             </Link>
-            <div className='nav-links-container'>
-                <Link className="nav-link" to='/shop'>
-                    SHOP
-                </Link>
-                <Link className="nav-link" to='/sign-in'>
-                    SIGN IN
-                </Link>
-            </div>
+            <Link className='nav-link' to='/auth'>
+              SIGN IN
+            </Link>
+          </div>
         </div>
         <Outlet />
-    </Fragment>
+      </Fragment>
     );
   };
-
-export default Navigation
+  
+  export default Navigation;
